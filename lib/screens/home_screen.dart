13c42a8eaa95a63355c79cb648475745204dc4c6
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2022-12-08 10:51:56 -0500

    Application in Flutter of Counter App

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  const HomeScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Text('Numero de clicks'),
+                Text('10')
+              ],
+            ),
+      ),
+    );
+  }
+
+}
